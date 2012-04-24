e49341fcb8a1709e2f0a27edded5b651c1069b25
Andrew Keig <andrew.keig@gmail.com>
2012-04-25 00:06:58 +0100

    0.0.8

--- routes/about.js
+++ routes/about.js
@@ -2,16 +2,14 @@ var konphyg = require('konphyg')('./config');
 var content_cfg = konphyg('content');
 
 exports.index = function(req, res){
-    var api =  { title: content_cfg.title};
+    var api = { title: content_cfg.title };
 
     res.format({
-        html: function(){
-            res.render('about', api);
-        },
-
         json: function(){
             res.json(api);
+        },
+        html: function(){
+            res.render('about', api);
         }
     })
-
 };
