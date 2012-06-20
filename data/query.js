3f3124d960db8d390a4df4e10fceef1f2dcd0ea5
Andrew Keig <andrew.keig@gmail.com>
2012-06-20 16:20:31 +0100

    0.1.2

--- data/query.js
+++ data/query.js
@@ -1,14 +1,16 @@
 var Article = require('./model').model('Article');
 
 exports.find_article_by_type = function (article_type, callback) {
-    console.log('- find by article type: ' + article_type);
-    Article.find({article_type: article_type}, function(err, article) {
-        if (err) {
-            callback(err);
-            return;
-        }
+    //console.log('- find by article type: ' + article_type);
+    //Article.find({article_type: article_type}, function(err, article) {
+    //    if (err) {
+    //        callback(err);
+    //        return;
+    //    }
 
-        callback(null, article);
-        return;
-    });
+    //    callback(null, article);
+    //    return;
+    //});
+
+    callback(null, null);
 }
