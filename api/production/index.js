b0baeb81c5a71cfad3d2cdf6c27981afb2a6cdee
Andrew Keig <andrew.keig@gmail.com>
2012-06-21 10:38:25 +0100

    0.1.2

--- api/production/index.js
+++ api/production/index.js
@@ -1,6 +1,6 @@
-exports.home = require('../api/home');
-exports.about = require('../api/about');
-exports.contact = require('../api/contact');
-exports.github = require('../api/github');
-exports.login = require('../api/login');
-exports.account = require('../api/account');
+exports.home = require('../api/production/home');
+exports.about = require('../api/production/about');
+exports.contact = require('../api/production/contact');
+exports.github = require('../api/production/github');
+exports.login = require('../api/production/login');
+exports.account = require('../api/production/account');
