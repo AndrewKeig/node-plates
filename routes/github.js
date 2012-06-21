8d0e94e8295782c4f4cd4290c8b03a1ffa5aedf9
Andrew Keig <andrew.keig@gmail.com>
2012-06-21 13:16:09 +0100

    0.1.2

--- routes/github.js
+++ routes/github.js
@@ -1,6 +1,8 @@
-var konphyg = require('konphyg')('./config')
+var path = require('path')
+    ,konphyg = require('konphyg')(path.join(__dirname, '../config'))
     ,express_cfg = konphyg('express')
     ,api = require(express_cfg.api);
+
 
 
 exports.index = function(req, res){
