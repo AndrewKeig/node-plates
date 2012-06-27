1b60c1a6146ad795bbf432270a1707bb980e10d7
Andrew Keig <andrew.keig@gmail.com>
2012-06-27 16:14:51 +0100

    0.1.2

--- public/javascripts/uri.js
+++ public/javascripts/uri.js
@@ -1,6 +1,6 @@
 var uri = 'http://127.0.0.1';
 var suri = 'https://127.0.0.1:8443';
-var uri_port = '8480';
+var uri_port = '8081';
 var suri_port = '8443';
 
 //var uri = 'http://nodeplates.airasoul.net';
